b0e0b2ce962d4f8a35fea388dea8d43503867331
victor <vicjosneros@gmail.com>
2023-10-01 13:05:52 -0600

    Modificacion al back, integracion de la api

--- back_zyxbot/config/cors.php
+++ back_zyxbot/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', "/prompt"],
 
     'allowed_methods' => ['*'],
 
--- back_zyxbot/routes/web.php
+++ back_zyxbot/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatApiController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::post("/prompt",[ChatApiController::class, "prompt"]);
+
+
+
+
 
 require __DIR__.'/auth.php';
